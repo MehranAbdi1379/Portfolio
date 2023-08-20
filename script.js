349ec3680a85b27898901aad08ea74d89f559604
Mehran Abdi <mehranabdi13793@gmail.com>
2023-08-20 13:59:44 +0330

    Configure and remake typewriter

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
-var typeWriterWords = ['Software Development', 'Web Development', 'ASP.Net Backe-End', 'Blazor/React Front-End']
+var typeWriterWords = [
+  "Software Development",
+  "Web Development",
+  "ASP.Net Backe-End",
+  "Blazor/React Front-End",
+];
+var selectedWord = typeWriterWords[0];
 var arrayWordCounter = 0;
 var wordLengthCounter = 0;
-var wordLengthCounterBackwards = typeWriterWords[0].length;
+var wordLengthCounterBackwards = selectedWord.length;
 var speed = 100;
 
 setInterval(typeWriterIntervar, speed);
 
 function typeWriterIntervar() {
-    if (i < typeWriterWords[0]) {
-        document.getElementById('home-typeWriter').innerHTML += txt.charAt(i);
-        i++;
+  if (wordLengthCounter < selectedWord.length + 5) {
+    document.getElementById("home-typeWriter").innerHTML = selectedWord.slice(
+      0,
+      wordLengthCounter
+    );
+    wordLengthCounter++;
+  } else if (wordLengthCounter == selectedWord.length + 5) {
+    if (wordLengthCounterBackwards > 0) {
+      document.getElementById("home-typeWriter").innerHTML = selectedWord.slice(
+        0,
+        wordLengthCounterBackwards
+      );
+    } else {
+      document.getElementById("home-typeWriter").innerHTML = " ";
     }
-    else {
-        if (j > 1) {
-            document.getElementById('home-typeWriter').innerHTML = document.getElementById('home-typeWriter').innerHTML.slice(0, j);
-        }
-        else {
-            document.getElementById('home-typeWriter').innerHTML = ' '
-        }
-        j--;
-    }
-    if (j == 0) {
-        i = 0;
-        j = txt.length;
-    }
+    wordLengthCounterBackwards--;
+  }
+  if (wordLengthCounterBackwards == 0 && arrayWordCounter == 3) {
+    arrayWordCounter = 0;
+    wordLengthCounter = 0;
+    selectedWord = typeWriterWords[arrayWordCounter];
+    wordLengthCounterBackwards = selectedWord.length;
+  } else if (wordLengthCounterBackwards == 0 && arrayWordCounter < 3) {
+    arrayWordCounter++;
+    wordLengthCounter = 0;
+    selectedWord = typeWriterWords[arrayWordCounter];
+    wordLengthCounterBackwards = selectedWord.length;
+  }
+  document.getElementById("home-typeWriter").innerHTML += "|";
 }
-
-
-
